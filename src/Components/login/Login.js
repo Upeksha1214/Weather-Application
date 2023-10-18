b8233a157115f1ae3f07330c958ec0a1a40dc73d
Upeksha1214 <upekshasachintha@gmail.com>
2023-10-18 11:19:58 -0700

    login in is complete

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -1,37 +1,125 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import styled from 'styled-components';
 
-const Login = ({ onLogin }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+const defaultTheme = createTheme();
 
-  const handleLogin = () => {
-    // Perform authentication logic here (e.g., check credentials against a database)
-    const isAuthenticated = true; // For this example, assume the user is authenticated
+const StyledHeading = styled.h1`
+  font-family: 'Arial', sans-serif; /* Specify your desired font-family */
+  font-size: 40px; /* Specify the font size */
+  color: #007bff; /* Specify the text color */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3); /* Apply a text shadow */
+  /* Add more styles as needed */
+`;
 
-    if (isAuthenticated) {
-      onLogin(); // Call the callback function passed from the parent component
-    } else {
-      // Handle authentication failure (e.g., show error message)
-    }
+const SignInSide = ({ onLogin }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const password = data.get('password');
+    onLogin(email, password); // Call the callback function passed from the parent component
   };
 
   return (
-    <div className="login-form">
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" style={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          style={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: 'lightgray',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            style={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+
+            <StyledHeading>WEATHER APP</StyledHeading>
+
+
+            <Avatar style={{ margin: 1, backgroundColor: '#f50057' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} style={{ marginTop: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                style={{ marginTop: 3, marginBottom: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
-export default Login;
+export default SignInSide;
